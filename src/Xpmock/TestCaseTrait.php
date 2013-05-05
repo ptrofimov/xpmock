11a6e3b5183e08c7918f4a33e04ec1d2d42405b6
Petr Trofimov <petrofimov@yandex.ru>
2013-05-05 18:49:22 +0300

    mock class code generation

--- src/Xpmock/TestCaseTrait.php
+++ src/Xpmock/TestCaseTrait.php
@@ -9,7 +9,23 @@ trait TestCaseTrait
             ->disableOriginalConstructor()
             ->getMock();
 
-        $mockClassName = $className . 'Mock';
+        $methods = [];
+        $class = new \ReflectionClass($className);
+        /** @var $method \ReflectionMethod */
+        foreach ($class->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
+            $methods[] = "public function {$method->getName()}(\$i){return call_user_func_array([\$this->mock,'{$method->getName()}'],func_get_args());}";
+            $methods[] = "public function mock{$method->getName()}(){return call_user_func_array([\$this,'__mock'],array_merge(['{$method->getName()}'],func_get_args()));}";
+
+        }
+
+        $className = $class->getShortName();
+        $mockClassName = $className . '_Mock_' . substr(md5(microtime(true)), 0, 4);
+        $methods = implode($methods);
+        $code = 'namespace ' . $class->getNamespaceName() . ";class $mockClassName extends $className{use \\Xpmock\\MockTrait;$methods}";
+
+        eval($code);
+
+        $mockClassName = $class->getNamespaceName() . '\\' . $mockClassName;
         $myMock = new $mockClassName($mock);
 
         return $myMock;
--- tests/stubs/My.php
+++ tests/stubs/My.php
@@ -8,24 +8,3 @@ class My
         return $i + 1;
     }
 }
-
-class MyMock extends My
-{
-    use \Xpmock\MockTrait;
-
-    public function getNumber($i)
-    {
-        return call_user_func_array(
-            [$this->mock, 'getNumber'],
-            func_get_args()
-        );
-    }
-
-    public function mockGetNumber()
-    {
-        return call_user_func_array(
-            [$this, '__mock'],
-            array_merge(['getNumber'], func_get_args())
-        );
-    }
-}
